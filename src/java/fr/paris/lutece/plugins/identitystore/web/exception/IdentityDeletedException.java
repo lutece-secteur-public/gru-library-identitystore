afe760e74551b2b3da163cc25af822d2db38ac74
TAGUETIEU <darelle.taguetieu.external@atos.net>
2018-11-22 11:43:02 +0100

    IDENTITY-70 : after formatter

--- src/java/fr/paris/lutece/plugins/identitystore/web/exception/IdentityDeletedException.java
+++ src/java/fr/paris/lutece/plugins/identitystore/web/exception/IdentityDeletedException.java
@@ -2,40 +2,41 @@ package fr.paris.lutece.plugins.identitystore.web.exception;
 
 import fr.paris.lutece.portal.service.util.AppException;
 
-public class IdentityDeletedException extends AppException {
-	
-	 private static final long serialVersionUID = 1L;
+public class IdentityDeletedException extends AppException
+{
 
-	    /**
-	     * Constructor 1
-	     *
-	     * @param strMessage
-	     *            The error message
-	     */
-	    public IdentityDeletedException( String strMessage )
-	    {
-	        super( strMessage );
-	    }
+    private static final long serialVersionUID = 1L;
 
-	    /**
-	     * Constructor 2
-	     *
-	     * @param strMessage
-	     *            The error message
-	     * @param e
-	     *            The initial exception
-	     */
-	    public IdentityDeletedException( String strMessage, Exception e )
-	    {
-	        super( strMessage, e );
-	    }
+    /**
+     * Constructor 1
+     *
+     * @param strMessage
+     *            The error message
+     */
+    public IdentityDeletedException( String strMessage )
+    {
+        super( strMessage );
+    }
 
-	    /**
-	     * Constructor 3
-	     */
-	    public IdentityDeletedException( )
-	    {
-	        super( );
-	    }
+    /**
+     * Constructor 2
+     *
+     * @param strMessage
+     *            The error message
+     * @param e
+     *            The initial exception
+     */
+    public IdentityDeletedException( String strMessage, Exception e )
+    {
+        super( strMessage, e );
+    }
+
+    /**
+     * Constructor 3
+     */
+    public IdentityDeletedException( )
+    {
+        super( );
+    }
 
 }
