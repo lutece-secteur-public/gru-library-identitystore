02335f99726ecc827cf4d07a691464f5b23fb08b
Guillaume Comminges <gcomminges@yupiik.com>
2024-12-16 10:37:09 +0100

    LUT-29102 [Identité] - Ajouter le type demande consultation demande rattachement identité connectée

--- src/java/fr/paris/lutece/plugins/identitystore/v3/web/rs/service/MockIdentityTransportRest.java
+++ src/java/fr/paris/lutece/plugins/identitystore/v3/web/rs/service/MockIdentityTransportRest.java
@@ -373,6 +373,12 @@ public class MockIdentityTransportRest implements IIdentityTransportProvider
     }
 
     @Override
+    public IdentityTaskListGetResponse getIdentityTaskListBySecondaryCuid(String secondaryCuid, String strClientCode, RequestAuthor author) throws IdentityStoreException
+    {
+        return null;
+    }
+
+    @Override
     public IdentityTaskSearchResponse searchIdentityTasks(IdentityTaskSearchRequest request, String strClientCode, RequestAuthor author) throws IdentityStoreException {
         return null;
     }
